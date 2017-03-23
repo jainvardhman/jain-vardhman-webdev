8858190e6e0772656192d1115f339ea2efb86ace
Vardhman Jain <jain.vardhman14@gmail.com>
2017-03-22 20:24:11 -0400

    adding code to connect heroku mongo db

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -5,6 +5,14 @@ module.exports = function(app) {
     var mongoose = require('mongoose');
     mongoose.connect("mongodb://localhost:27017/test");
 
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
     var pageModel = require("./page/page.model.server")();
